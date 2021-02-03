53ee320ffb59e105e8f8e2a6656d2faccff5e8f1
Syed Areeb <syedraeebshah806@gmail.com>
2021-02-03 21:21:17 +0500

    fb auth.. done

--- AwesomeProject/screens/Log.js
+++ AwesomeProject/screens/Log.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import auth from '@react-native-firebase/auth';
 import onPress from './Location'
-
+import { LoginManager, AccessToken } from "react-native-fbsdk";
 import {
   Image,
   ScrollView,
@@ -15,39 +16,77 @@ import Geolocation from '@react-native-community/geolocation';
 
 
 
+  
+  
 
-
-const Log = ({ navigation }) => {
-  onPress()
-  function login() {
-    navigation.navigate('Home')
-    console.log("login")
-    Geolocation.getCurrentPosition(info => console.log(info));
+  let Log = ({ navigation }) => {
     
+function jpg(){
+  console.log(".....")
+}
 
-  }
+
+    onPress()
+    function login() {
+      navigation.navigate('Home')
+      console.log("login")
+      Geolocation.getCurrentPosition(info => console.log(info));
+    }
+
+    const fbLoging = async () => {
+      const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+  
+      if (result.isCancelled) {
+        throw 'User cancelled the login process';
+      }
+  
+      // Once signed in, get the users AccesToken
+      const data = await AccessToken.getCurrentAccessToken();
+  
+      if (!data) {
+        throw 'Something went wrong obtaining access token';
+      }
+  
+      // Create a Firebase credential with the AccessToken
+      const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+  
+      // Sign-in the user with the credential
+      let userLogin = auth().signInWithCredential(facebookCredential)
+      .then((user)=>{
+        console.log("ok")
+        login()
+      })
+      .catch((err)=>{
+        alert("Something went wrong")
+      })
+    }
+
+
+
+
+
   return (
     <ScrollView style={styles.main}>
       <View style={styles.one}>
 
         <Image style={styles.logo} source={require('./assets/logo.png')} />
 
-        <Item style={{ margin: 10 }} rounded>
+        {/* <Item style={{ margin: 10 }} rounded>
           <Input placeholder='Enter Email' />
         </Item>
         <Item style={{ margin: 10 }} rounded>
           <Input placeholder='Enter Password' />
-        </Item>
+        </Item> */}
 
-        <Button onPress={() => login()} style={{ margin: 5, }} block>
-          <Text style={{ color: 'white', }}>Login </Text>
+        <Button onPress={() => fbLoging()} style={{ margin: 5, }} block>
+          <Text style={{ color: 'white', }}>Login With Facebook </Text>
         </Button>
 
-        <Text style={{ color: 'black', fontSize: 20 }}> Don't have account? Sign Up </Text>
+        {/* <Text style={{ color: 'black', fontSize: 20 }}> Don't have account? Sign Up </Text>
 
         <Button onPress={() => navigation.navigate('Sinup')} style={{ margin: 5, width: '40%', alignSelf: 'center' }} block>
           <Text style={{ color: 'white', }}>Sign up </Text>
-        </Button>
+        </Button> */}
 
 
       </View>
